f99710db1a6fd28d7dd3e2ef7afae920bd60718d
Andy Stechishin <andy.stechishin@gmail.com>
2015-08-13 18:08:22 -0600

    Data construction
    Begin work on Thor tasks to construct database from Excel data

---
+++ db/schema.rb
@@ -0,0 +1,92 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140514171022) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "ballot_cmets", force: true do |t|
+    t.integer "ballot_id",              null: false
+    t.integer "cmet_id",                null: false
+    t.string  "disposition", limit: 10, null: false
+  end
+
+  create_table "ballot_cycles", primary_key: "cycle", force: true do |t|
+    t.date     "open"
+    t.date     "close"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ballots", force: true do |t|
+    t.integer  "work_group_id",                  null: false
+    t.string   "ballot_cycle",        limit: 7,  null: false
+    t.string   "level",               limit: 10
+    t.string   "result",              limit: 10
+    t.date     "result_date"
+    t.string   "reconciliation_path"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "cmet_version_notes", force: true do |t|
+    t.integer  "cmet_version_id", null: false
+    t.text     "note",            null: false
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "cmet_versions", force: true do |t|
+    t.string   "cmet_id",     limit: 13, null: false
+    t.string   "realm",       limit: 10
+    t.string   "release",     limit: 3
+    t.string   "status",      limit: 10
+    t.date     "status_date"
+    t.text     "notes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "cmets", primary_key: "ident", force: true do |t|
+    t.string   "descriptor",    limit: 50
+    t.string   "attribution",   limit: 30
+    t.text     "description"
+    t.integer  "work_group_id"
+    t.string   "domain",        limit: 2
+    t.boolean  "retired",                  default: false, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "cmets", ["ident"], name: "index_cmets_on_ident", unique: true, using: :btree
+
+  create_table "code_values", force: true do |t|
+    t.string "table",      limit: 10
+    t.string "code",       limit: 10
+    t.string "long_value", limit: 25
+    t.string "full_value", limit: 100
+  end
+
+  add_index "code_values", ["table", "code"], name: "index_code_values_on_table_and_code", unique: true, using: :btree
+
+  create_table "work_groups", force: true do |t|
+    t.string   "acronym",    limit: 6
+    t.string   "name",       limit: 50,                null: false
+    t.boolean  "active",                default: true, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
---
+++ groom-data.thor
@@ -0,0 +1,42 @@
+require "csv"
+
+class GroomData < Thor
+  desc 'generate_versions SOURCE_CSV_FILE',
+    'Process the CSV input to generate an output CSV containing unique CMET and version'
+  method_option :out, type: :string, default: 'db/load_data/input-data/01-02-cmet_versions.csv'
+  def generate_versions(csv_file)
+    cmets = {}
+    the_date = Date.today
+
+    CSV.foreach(csv_file, :headers => true) do |row|
+      unless cmets.has_key?(row[0])
+        cmets[row[0]] = {}
+      end
+      cmet = cmets[row[0]]
+
+      cmet[row[3]] = {ballot: row[2], status: row[5], notes: row[6]}
+    end
+
+    new_csv = CSV.open(options[:out], "w")
+    id = 1
+    cmets.each_pair do |cmet, releases|
+      releases.each_pair do |release, rel_data|
+        ballot_yr = rel_data[:ballot][0..3]
+        ballot_cycle = rel_data[:ballot][4..6]
+        puts "#{cmet}--#{release} #{ballot_yr}-#{ballot_cycle}"
+        #id, cmet_id,realm,release,status,status_date,notes,created,updated
+        new_csv << [id, cmet, 'UV', "#{ballot_yr}-#{ballot_cycle}",rel_data[:status] ]
+        id += 1
+      end
+    end
+
+    new_csv.close
+  end
+
+  desc 'generate_ballots SOURCE_CSV_FILE',
+    'Process the CSV input file to generate a CSV list of actual ballots for the supplied WorkGroup'
+  method_option :work_group, type: :string, default: "MnM"
+  def generate_ballots(csv_file)
+
+  end
+end
